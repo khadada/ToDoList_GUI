02995c3a38dace7d6fca079151dded30d274523d
khadada <lkhadada@gmail.com>
2022-04-01 17:29:10 +0200

    I imported necessary modules and classes for ToDoList Gui

---
+++ todolist.py
@@ -0,0 +1,5 @@
+# import necessary modules and classes
+import sys
+from PyQt5.QtWidgets import (QWidget,QApplication,QLabel,QTextEdit,QLineEdit,QPushButton,QCheckBox,QGridLayout,QVBoxLayout)
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
